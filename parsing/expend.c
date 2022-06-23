2927ba7e295a9be7219d75762e5a6cf18149f71a
Manutea Matthieu <mmatthie@c2r6p2.42nice.fr>
2022-06-23 18:43:35 +0200

    need to fix some parasit characer on the $

--- parsing/expend.c
+++ parsing/expend.c
@@ -6,6 +6,7 @@ int	get_expend(char	*str, int count, t_data	*data)
 	int	i;
 
 	tmp = count;
+	count++;
 	if (str[tmp] == '$')
 	{
 		if (str[tmp + 1] == '$')
@@ -14,12 +15,15 @@ int	get_expend(char	*str, int count, t_data	*data)
 		while (str[++tmp] != '\'' && str[tmp] != '"' && str[tmp])
 			i++;
 		data->get_str_env = ft_calloc(1, i);
-		data->get_str_env = ft_strncpy(data->get_str_env, &str[count + 1], i);
+		data->get_str_env = ft_strncpy(data->get_str_env, &str[count], i);
 		data->get_env = getenv(data->get_str_env);
 		if (data->get_env)
 		{
+			printf("data->get_env : %s\n", data->get_env);
 			data->get_word = ft_calloc(1, ft_strlen(data->get_env));
 			data->get_word = ft_strncpy(data->get_word, data->get_env, ft_strlen(data->get_env));
+			printf("ft_strlen(data->get_env) : %zu\n", ft_strlen(data->get_env));
+			printf("data->get_word : %s\n", data->get_word);
 		}
 		else
 			data->get_word = NULL;
